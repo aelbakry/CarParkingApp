ef156467872601eb4e576606a6f54167e16d51c5
Ahmed Mohamed <ahmed.sherif1999@hotmail.com>
2018-10-27 15:31:38 +0100

    Fields and links created for account view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,24 @@
 class ApplicationController < ActionController::Base
 
-  def home
-  end
+  # Prevent CSRF attacks by raising an exception.
+    # For APIs, you may want to use :null_session instead.
+    protect_from_forgery with: :exception
 
-  def display
-  end
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+    protected
+
+        def configure_permitted_parameters
+            devise_parameter_sanitizer.permit(:sign_up, keys: [:surname, :forename, :email, :password])
+            devise_parameter_sanitizer.permit(:account_update, keys: [:surname, :forename, :email, :password, :current_password])
+
+        end
+
+  # def home
+  # end
+  #
+  # def display
+  # end
 
 end
